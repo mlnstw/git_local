ff3b9224bf684e6959702442b34cffbe27bc7978
mlnstw <sammygsn@gmail.com>
2025-02-06 03:06:34 +0300

    dev, + ansible && terraform

---
+++ terraform/terraform-test/main.tf
@@ -0,0 +1,38 @@
+#docker rm -f $(docker ps -a -q)
+#terraform console
+#curl -L  http://127.0.0.1:1080 ; curl -L  http://127.0.0.1:2080
+
+
+resource "docker_image" "nginx" {
+  name         = var.containers.nginx.image
+  keep_locally = true
+}
+
+resource "docker_image" "wordpress" {
+  name         = var.containers.wordpress.image
+  keep_locally = true
+}
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "our-cool-project-${var.containers.nginx.name}" #interpolation
+
+  ports {
+    internal = var.containers.nginx.ports.internal
+    external = var.containers.nginx.ports.external
+  }
+}
+
+resource "docker_container" "wordpress" {
+  image = docker_image.wordpress.image_id
+  name  = "our-cool-project-${var.containers.wordpress.name}" #interpolation
+
+  ports {
+    internal = var.containers.wordpress.ports.internal
+    external = var.containers.wordpress.ports.external
+  }
+}
+
+resource "local_file" "xxx" {
+  content  = "our-cool-project-${var.containers.wordpress.name}"
+  filename = "/tmp/xxx.txt"
+}
